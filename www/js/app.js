e89fd93fac53b3ebfe3cc7901865b0ec26cfdc0d
MarcosLopez7 <marcos.lopezmendoza@gmail.com>
2016-02-19 18:49:45 -0600

    A la carga

---
+++ www/js/app.js
@@ -0,0 +1,48 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+(function(){
+    var app = angular.module('starter', ['ionic']);
+    
+    
+    app.config(function($stateProvider, $urlRouterProvider){
+       $stateProvider.state('logging', {
+            'url': '/logging',
+            'templateUrl': 'templates/logging.html'
+       });
+    
+        $stateProvider.state('opcion', {
+             'url': '/opcion',
+             'templateUrl': 'templates/opcion.html'
+        });
+        
+        $stateProvider.state('menudia', {
+              'url': '/menudia',
+              'templateUrl': 'templates/menudia.html'
+        });
+        
+       $urlRouterProvider.otherwise('/logging')
+        
+    });
+
+    app.run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+                if(window.cordova && window.cordova.plugins.Keyboard) {
+                    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+                    // for form inputs)
+                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+                    // Don't remove this line unless you know what you are doing. It stops the viewport
+                    // from snapping when text inputs are focused. Ionic handles this internally for
+                    // a much nicer keyboard experience.
+                    cordova.plugins.Keyboard.disableScroll(true);
+                }
+                
+                if(window.StatusBar) {
+                    StatusBar.styleDefault();
+                }
+        });
+    })
+}());
